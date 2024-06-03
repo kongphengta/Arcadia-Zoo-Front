3a566cef6dead5ccc607aeb4119e93494628248c
kongphengta <kong.vixay@gmail.com>
2024-06-03 19:52:14 +0200

    Add CustomerInterface

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,6 +6,8 @@ export const allRoutes = [
     new Route("/animaux", "Animaux", "/pages/animaux.html"),
     new Route("/habitats", "Habitats", "/pages/habitats.html"),
     new Route("/panda", "Panda", "/pages/panda.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/signup", "Inscription", "/pages/signup.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
